17510ba0aa22a01ad04a1a4005f26b0b18837eff
Heejin Kim <dev.hannahk@gmail.com>
2021-09-05 19:52:27 +0900

    create add and delete mutation

--- graphql/db.js
+++ graphql/db.js
@@ -1,31 +1,49 @@
-export const people = [
+let movies = [
   {
-    id: "1",
-    name: "Hannah",
-    age: 20,
-    gender: "female",
+    id: 1,
+    name: "Avengers - The new one",
+    score: 8,
   },
   {
-    id: "2",
-    name: "Jenna",
-    age: 20,
-    gender: "female",
+    id: 2,
+    name: "Midnight Library",
+    score: 7,
   },
   {
-    id: "3",
-    name: "Alice",
-    age: 20,
-    gender: "female",
+    id: 3,
+    name: "About Time",
+    score: 9,
   },
   {
-    id: "4",
-    name: "Jean",
-    age: 20,
-    gender: "female",
+    id: 4,
+    name: "Intern",
+    score: 8,
   },
 ];
 
+export const getMovies = () => movies;
+
 export const getById = (id) => {
-  const filteredPeople = people.filter((person) => person.id === String(id));
-  return filteredPeople[0];
+  const filteredMovies = movies.filter((movie) => movie.id === id);
+  return filteredMovies[0];
+};
+
+export const deleteMovie = (id) => {
+  const cleanedMovies = movies.filter((movie) => movie.id !== id);
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: `${movies.length + 1}`,
+    name,
+    score,
+  };
+  movies.push(newMovie);
+  return newMovie;
 };
